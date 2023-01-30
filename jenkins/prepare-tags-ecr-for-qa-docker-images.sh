d6192d8a0d430e689068e3013551aed68c7b94a5
usuladams <usul.adem@gmail.com>
2023-01-30 21:12:14 +0000

    added jenkinsfile petclinic-weekly-qa for release branch

--- jenkins/prepare-tags-ecr-for-qa-docker-images.sh
+++ jenkins/prepare-tags-ecr-for-qa-docker-images.sh
@@ -1,6 +1,6 @@
-MVN_VERSION=sh(script:'''. ${WORKSPACE}/spring-petclinic-admin-server/target/maven-archiver/pom.properties && echo $version''', returnStdout:true).trim()
+MVN_VERSION=sh(script:". ${WORKSPACE}/spring-petclinic-admin-server/target/maven-archiver/pom.properties && echo $version", returnStdout:true).trim()
 env.IMAGE_TAG_ADMIN_SERVER="${ECR_REGISTRY}/${APP_REPO_NAME}:admin-server-qa-v${MVN_VERSION}-b${BUILD_NUMBER}"
-MVN_VERSION=sh(script:'''. ${WORKSPACE}/spring-petclinic-api-gateway/target/maven-archiver/pom.properties && echo $version''', returnStdout:true).trim()
+MVN_VERSION=sh(script:". ${WORKSPACE}/spring-petclinic-api-gateway/target/maven-archiver/pom.properties && echo $version", returnStdout:true).trim()
 env.IMAGE_TAG_API_GATEWAY="${ECR_REGISTRY}/${APP_REPO_NAME}:api-gateway-qa-v${MVN_VERSION}-b${BUILD_NUMBER}"
 MVN_VERSION=sh(script:'''. ${WORKSPACE}/spring-petclinic-config-server/target/maven-archiver/pom.properties && echo $version''', returnStdout:true).trim()
 env.IMAGE_TAG_CONFIG_SERVER="${ECR_REGISTRY}/${APP_REPO_NAME}:config-server-qa-v${MVN_VERSION}-b${BUILD_NUMBER}"
